5401d33231af6d438d9464514d6b95c7593018b5
myacxy <myacxy@gmail.com>
2014-11-12 23:36:28 +0100

    mvvm select main tab from navigation

--- UniMoveStationMvvm/ViewModel/NavigationViewModel.cs
+++ UniMoveStationMvvm/ViewModel/NavigationViewModel.cs
@@ -18,6 +18,90 @@ namespace UniMoveStation.ViewModel
     {
         private CompositeCollection _cameras;
         private ObservableCollection<SingleCameraViewModel> _singleCameras;
+        private RelayCommand<int> _selectControllerCommand;
+        private RelayCommand<int> _selectCameraCommand;
+        private int _mainSelectedIndex = 0;
+        private int _controllerSelectedIndex = 0;
+        private int _cameraSelectedIndex = 0;
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the NavigationViewModel class.
+        /// </summary>
+        [PreferredConstructor]
+        public NavigationViewModel()
+        {
+            Cameras = new CompositeCollection();
+
+            SingleCameras.Add(new SingleCameraViewModel(0));
+            SingleCameras.Add(new SingleCameraViewModel(1337));
+            SingleCameras.Add(new SingleCameraViewModel(1));
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// The <see cref="MainSelectedIndex" /> property's name.
+        /// </summary>
+        public const string MainSelectedIndexPropertyName = "MainSelectedIndex";
+
+        /// <summary>
+        /// Sets and gets the MainSelectedIndex property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public int MainSelectedIndex
+        {
+            get
+            {
+                return _mainSelectedIndex;
+            }
+            set
+            {
+                Set(MainSelectedIndexPropertyName, ref _mainSelectedIndex, value);
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="ControllerSelectedIndex" /> property's name.
+        /// </summary>
+        public const string ControllerSelectedIndexPropertyName = "ControllerSelectedIndex";
+
+        /// <summary>
+        /// Sets and gets the ControllerSelectedIndex property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public int ControllerSelectedIndex
+        {
+            get
+            {
+                return _controllerSelectedIndex;
+            }
+            set
+            {
+                Set(ControllerSelectedIndexPropertyName, ref _controllerSelectedIndex, value);
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="CameraSelectedIndex" /> property's name.
+        /// </summary>
+        public const string CameraSelectedIndexPropertyName = "CameraSelectedIndex";
+
+        /// <summary>
+        /// Sets and gets the CameraSelectedIndex property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public int CameraSelectedIndex
+        {
+            get
+            {
+                return _cameraSelectedIndex;
+            }
+            set
+            {
+                Set(CameraSelectedIndexPropertyName, ref _cameraSelectedIndex, value);
+            }
+        }
 
         public CompositeCollection Cameras
         {
@@ -61,50 +145,44 @@ namespace UniMoveStation.ViewModel
                 return SimpleIoc.Default.GetInstance<AllCamerasViewModel>();
             }
         }
+        #endregion
 
+        #region Relay Commands
         /// <summary>
-        /// Initializes a new instance of the NavigationViewModel class.
+        /// Gets the SelectControllerCommand.
         /// </summary>
-        [PreferredConstructor]
-        public NavigationViewModel()
+        public RelayCommand<int> SelectControllerCommand
         {
-            Cameras = new CompositeCollection();
-            SelectTabCommand = new RelayCommand<MetroTabItem>(DoSelectTab);
-            AddCameraCommand = new RelayCommand(DoAddCamera);
-
-            SingleCameras.Add(new SingleCameraViewModel(0));
-            SingleCameras.Add(new SingleCameraViewModel(1337));
-            SingleCameras.Add(new SingleCameraViewModel(1));
-        }
-
-        /// <summary>
-        /// Gets the SelectTabCommand.
-        /// </summary>
-        public RelayCommand<MetroTabItem> SelectTabCommand
-        {
-            get;
-            private set;
-        }
-
-        private void DoSelectTab(MetroTabItem tabItem)
-        {
-            tabItem.IsSelected = true;
-            MetroTabItem parent = (MetroTabItem)tabItem.OwningTabControl.Parent;
-            parent.IsSelected = true;
+            get
+            {
+                return _selectControllerCommand
+                    ?? (_selectControllerCommand = new RelayCommand<int>(DoSelectControllerCommand));
+            }
         }
 
         /// <summary>
-        /// Gets the AddCameraCommand.
+        /// Gets the SelectControllerCommand.
         /// </summary>
-        public RelayCommand AddCameraCommand
+        public RelayCommand<int> SelectCameraCommand
         {
-            get;
-            private set;
+            get
+            {
+                return _selectCameraCommand
+                    ?? (_selectCameraCommand = new RelayCommand<int>(DoSelectCameraCommand));
+            }
         }
 
-        private void DoAddCamera()
+        private void DoSelectControllerCommand(int index)
         {
-            Cameras.Add(new SingleCameraModel());
+            MainSelectedIndex = 0;
+            ControllerSelectedIndex = index + 1;
         }
+
+        private void DoSelectCameraCommand(int index)
+        {
+            MainSelectedIndex = 1;
+            CameraSelectedIndex = index + 1;
+        }
+        #endregion
     }
 }
