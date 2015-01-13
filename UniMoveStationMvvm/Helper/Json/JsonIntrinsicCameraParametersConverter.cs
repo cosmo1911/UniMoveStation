971fafcb6b6c24baba57bfe28063256f28a3f21c
myacxy <myacxy@gmail.com>
2015-01-13 22:09:47 +0100

    [wip] intrinsic parameters json converter

---
+++ UniMoveStationMvvm/Helper/Json/JsonIntrinsicCameraParametersConverter.cs
@@ -0,0 +1,38 @@
+﻿using Emgu.CV;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniMoveStation.Helper.Json
+{
+    public class JsonIntrinsicCameraParametersConverter : JsonCreationConverter<IntrinsicCameraParameters>
+    {
+
+        protected override Type GetType(Type objectType, JObject jsonObject)
+        {
+            var type = (string) jsonObject.Property("IntrinsicMatrix");
+            if (type != null)
+            {
+                return typeof(IntrinsicCameraParameters);
+            }
+            else
+            {
+                throw new ApplicationException(String.Format("The given type {0} is not supported!", type));
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return base.ReadJson(reader, objectType, existingValue, serializer);
+        }
+    } // JsonIntrinsicCameraParametersConverter
+} // namespace
